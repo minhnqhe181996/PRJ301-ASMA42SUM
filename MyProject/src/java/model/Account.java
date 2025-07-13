52810089e45cd5a2632a525335335efa37caa5c8
Nguyễn Quang Minh <minhnqhe181996@fpt.edu.vn>
2025-07-14 00:30:43 +0700

    tạo các thuộc tính cho Account
    tạo các thuộc tính cho Account

--- MyProject/src/java/model/Account.java
+++ MyProject/src/java/model/Account.java
@@ -9,5 +9,59 @@ package model;
  * @author quang
  */
 public class Account {
+    private int Id;
+    private String Username;
+    private String Password;
+    private int EmployeeId;
+    private int RoleId;
+
+    public Account(int Id, String Username, String Password, int EmployeeId, int RoleId) {
+        this.Id = Id;
+        this.Username = Username;
+        this.Password = Password;
+        this.EmployeeId = EmployeeId;
+        this.RoleId = RoleId;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public int getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(int EmployeeId) {
+        this.EmployeeId = EmployeeId;
+    }
+
+    public int getRoleId() {
+        return RoleId;
+    }
+
+    public void setRoleId(int RoleId) {
+        this.RoleId = RoleId;
+    }
+    
     
 }
