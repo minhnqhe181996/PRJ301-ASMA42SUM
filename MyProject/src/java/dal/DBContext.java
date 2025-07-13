4c508ab2c206573edc2d3acc9ed6f99078ff10fe
Nguyễn Quang Minh <minhnqhe181996@fpt.edu.vn>
2025-07-14 00:14:40 +0700

    create my Project Folder
    create my Project Folder

---
+++ MyProject/src/java/dal/DBContext.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class DBContext {
+    
+}
