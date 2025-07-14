8e9f3bd15204a10dfd61617975cbbb6b986f58e4
Nguyễn Quang Minh <minhnqhe181996@fpt.edu.vn>
2025-07-14 10:50:38 +0700

    tạo mấy cái DAO

---
+++ MyProject/src/java/dal/AccountDAO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+import model.Account;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author quang
+ */
+public class AccountDAO {
+    
+}
