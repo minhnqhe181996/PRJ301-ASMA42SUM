0efc2e2f62cfd05a419f24fea6660add80f99f23
Nguyễn Quang Minh <minhnqhe181996@fpt.edu.vn>
2025-07-14 14:55:04 +0700

    tạo 3 cái AgendaDTO, ScheduleDTO, RequestDTO
    DTO gom dữ liệu từ nhiều bảng
    
    DTO còn có thể định dạng thêm (ví dụ: status = "✔ Approved")
    AgendaDTO	Hiển thị lịch làm việc theo tên nhân viên
    ScheduleDTO	(Nếu có) gom thông tin mở rộng từ Schedule + Employee
    RequestDTO	Hiển thị yêu cầu nghỉ phép cùng tên nhân viên

---
+++ MyProject/src/java/model/AgendaDTO.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+import java.util.List;
+
+public class AgendaDTO {
+    private int eId;
+    private String eName;
+        
+    // Thêm trường danh sách RequestDTO:
+    private List<RequestDTO> requests;
+
+    public AgendaDTO() {
+    }
+
+    public AgendaDTO(int eId, String eName, List<RequestDTO> requests) {
+        this.eId = eId;
+        this.eName = eName;
+        this.requests = requests;
+    }
+
+    public int geteId() {
+        return eId;
+    }
+
+    public void seteId(int eId) {
+        this.eId = eId;
+    }
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    public List<RequestDTO> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(List<RequestDTO> requests) {
+        this.requests = requests;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "AgendaDTO{" + "eId=" + eId + ", eName=" + eName + ", requests=" + requests + '}';
+    }
+    
+}
