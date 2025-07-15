40ea6fd6ccc2b89cc16613527572e6a7d2bf4b5d
Nguyễn Quang Minh <minhnqhe181996@fpt.edu.vn>
2025-07-15 19:09:40 +0700

    Update LoginControll.java

--- MyProject/src/java/controller/LoginControll.java
+++ MyProject/src/java/controller/LoginControll.java
@@ -4,10 +4,45 @@
  */
 package controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import dal.AccountDAO;
+import dal.DBContext;
+import model.Account;
+import java.sql.*;
 /**
  *
  * @author quang
  */
-public class LoginControll {
-    
+import jakarta.servlet.http.HttpSession;
+
+public class LoginControll extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        String u = req.getParameter("username");
+        String p = req.getParameter("password");
+
+        AccountDAO accountDAO = new AccountDAO();
+        Account acc = accountDAO.validateUser(u, p);
+
+        if (acc == null) {
+            resp.sendRedirect("Login.jsp?error=1");
+        } else {
+            HttpSession session = req.getSession();
+            session.setAttribute("account", acc);
+            resp.sendRedirect("home");
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
 }
