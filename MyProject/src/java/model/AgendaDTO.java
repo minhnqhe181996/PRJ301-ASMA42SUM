8cf50356ad9acb71f63f03af6950abfebbaaceae
Nguyễn Quang Minh <minhnqhe181996@fpt.edu.vn>
2025-07-15 18:10:36 +0700

    Update AgendaDTO.java

--- MyProject/src/java/model/AgendaDTO.java
+++ MyProject/src/java/model/AgendaDTO.java
@@ -7,47 +7,18 @@ package model;
 import java.util.Date;
 import java.util.List;
 
+/**
+ *
+ * @author ADMIN
+ */
 public class AgendaDTO {
     private int eId;
     private String eName;
-    // Existing requests field
+    
+    
     private List<RequestDTO> requests;
 
-    // Fields for schedule info
-    private int id;
-    private int employeeId;
-    private Date date;
-    private boolean status;
-
     public AgendaDTO() {
-    }
-    // Getters and setters for schedule info
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getEmployeeId() {
-        return employeeId;
-    }
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-    public void setStatus(boolean status) {
-        this.status = status;
     }
 
     public AgendaDTO(int eId, String eName, List<RequestDTO> requests) {
@@ -80,10 +51,4 @@ public class AgendaDTO {
         this.requests = requests;
     }
 
-   
-    @Override
-    public String toString() {
-        return "AgendaDTO{" + "eId=" + eId + ", eName=" + eName + ", requests=" + requests + '}';
-    }
-    
 }
